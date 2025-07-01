42ef0612fda1741bfd262dd298cc033649f1ef38
Umar <umarsabirin369@gmail.com>
2025-07-01 19:22:44 +0800

    Init project

---
+++ src/main/java/com/mockpilot/mockery_spring/MockerySpringApplication.java
@@ -0,0 +1,13 @@
+package com.mockpilot.mockery_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockerySpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockerySpringApplication.class, args);
+	}
+
+}
